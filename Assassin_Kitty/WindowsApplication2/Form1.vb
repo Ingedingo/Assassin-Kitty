bbee81e10b408656e380447fafa022a85a5ad387
Ingedingo <50899639+Ingedingo@users.noreply.github.com>
2019-05-21 22:09:21 -0700

    Add files via upload

---
+++ Assassin_Kitty/WindowsApplication2/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub Button1_Click() Handles Button1.Click
+        MessageBox.Show("Press okay to pull the trigger", "Kitty")
+        Shell("CMD.exe /C start chrome --new-window https://www.youtube.com/watch?v=hNEJfMnAoxo")
+        MessageBox.Show("Mission Failure better luck next time", "Dead Kitty")
+        Shell("CMD.exe /C start chrome --new-window https://www.youtube.com/watch?v=dwLCjZVEtpE")
+    End Sub
+End Class
